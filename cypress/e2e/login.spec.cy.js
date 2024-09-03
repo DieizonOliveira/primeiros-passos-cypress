c7b503fac1f4d67ca77b193a2511a540f240bffc
DIEIZON_OLIVEIRA <dieizonos@gmail.com>
2024-09-03 02:30:20 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,19 +1,37 @@
 describe('Orange HRM Tests', () => {
-  it.skip('Login - Success', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']" 
+
+
+  }
+
+  it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
   })
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Teste')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Teste')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert')
+    cy.get(selectorList.usernameField).type('Teste')
+    cy.get(selectorList.passwordField).type('Teste')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
   })
 })
-// Os seletores grandes foram feitos direto com o cypres e para achar os menores usamos o css selector.
+
+
+//  1- Os seletores maiores inicialmente foram feitos direto com o cypres e para achar os menores usamos o css selector.
+//Exemplo: cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Teste')
+//  2 - Após fomos para seletores menores com ajuda do css Selector:
+// Exemplo: cy.get("[name='username']").type('Admin') (Estes agora estão no objeto)
+//  3 - Depois passamos a utilizar objeto para facilitar uma possivel alteração e não termos repetição de código.
+// SKIP: it.skip('Login - Success', () => {... Serve para pular a execução de um teste quando desejado.
